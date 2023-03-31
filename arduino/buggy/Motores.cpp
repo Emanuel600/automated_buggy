08c572409084b2f8dd78eeb2e720def7e3d13869
Emanuel <78040463+Emanuel600@users.noreply.github.com>
2023-03-31 16:31:15 -0300

    Add files via upload

--- arduino/buggy/Motores.cpp
+++ arduino/buggy/Motores.cpp
@@ -9,18 +9,26 @@
 
       C
 
-  M1N -  M2N
-
-  Motores MxN estão INVERTIDOS (sentido anti-horário de rotação)
+      w
+   \=====/  => 13.65 cm de largura do eixo
+            => 6.825 cm de eixo com relação ao centro de massa
+            => Roda de ~6.5 cm de comprimento
+            => ~12 cm de comprimento
+  Motores MxN estão simplesmente INVERTIDOS, ligando Vcc em GND e vice-versa
   Pinout (Conforme definido no enum):
-  M1 : 6
-  M1N: 5
+  M1 : 6  => Pin 1
+  M1N: 5  => Pin 2
   --
-  M2 : 11
-  M2N: 3
+  M2 : 11 => Pin 4
+  M2N: 3  => Pin 3
+
+  Controla a ponte H (driver - L298N), não os motores. Enables não serão utilizados
 */
 
 #include "Motores.h"
+
+#define RAIO_ROBO 6.825
+#define CONV_FACT 2.5
 
 Motores::Motores()
 {
@@ -46,11 +54,10 @@ void Motores::frente(unsigned char velocidade){
 
   /*digitalWrite(M1, HIGH);  
   digitalWrite(M2, HIGH); */
-  velocidade &= 0xff; // Retorna valor igual a velocidade? > checar
+  velocidade &= 0xff; // Retorna valor igual a velocidade? > checar com professor
 
   analogWrite(M1, velocidade);
   analogWrite(M2, velocidade);
-
 }
 
 void Motores::tras(unsigned char velocidade){
@@ -62,10 +69,9 @@ void Motores::tras(unsigned char velocidade){
 
   analogWrite(M1N, velocidade);
   analogWrite(M2N, velocidade);
-
 }
-
-void Motores::direita(unsigned char velocidade){
+// Horário
+void Motores::horario(unsigned char velocidade){
   parar();
   velocidade &= 0xff;
   
@@ -74,17 +80,13 @@ void Motores::direita(unsigned char velocidade){
 
   analogWrite(M1, velocidade);
   analogWrite(M2N, velocidade);
-
-  
 }
-
-void Motores::esquerda(unsigned char velocidade){
+// Antihorário
+void Motores::anti_hora(unsigned char velocidade){
   parar();
   velocidade &= 0xff;
 
   /* digitalWrite(M2N, HIGH);
   digitalWrite(M1, HIGH);  */
 
-  analogWrite(M2, velocidade);
-  analogWrite(M1N, velocidade);
-}
+  analogWrite(M2,
